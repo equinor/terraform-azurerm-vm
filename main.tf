c3d823df195368d6afe5fed0460cd62934c60556
Stian Rossland <94676511+stiross79@users.noreply.github.com>
2024-03-01 10:09:41 +0100

    Added initial config

--- main.tf
+++ main.tf
@@ -1,1 +1,100 @@
+# Linux VM
+resource "azurerm_linux_virtual_machine" "this" {
+  count                           = var.host.is_windows ? 0 : 1
+  resource_group_name             = var.resource_group_name
+  name                            = var.host.name
+  computer_name                   = coalesce(var.host.computer_name, var.host.name)
+  size                            = var.host.size
+  location                        = var.location
+  disable_password_authentication = false
+  admin_username                  = "imadmin"
+  admin_password                  = random_password.this.result
+  tags                            = var.tags
+  custom_data                     = try(var.custom_data, null)
 
+  bypass_platform_safety_checks_on_user_schedule_enabled = true
+  provision_vm_agent                                     = true
+
+  network_interface_ids = [
+    azurerm_network_interface.this.id
+  ]
+
+  source_image_reference {
+    publisher = try(var.source_image.publisher, "canonical")
+    offer     = try(var.source_image.offer, "0001-com-ubuntu-server-focal")
+    sku       = try(var.source_image.sku, "20_04-lts-gen2")
+    version   = try(var.source_image.version, "latest")
+  }
+
+  identity {
+    type         = length(var.user_assigned_identity_ids) != 0 ? "SystemAssigned, UserAssigned" : "SystemAssigned"
+    identity_ids = var.user_assigned_identity_ids
+  }
+
+  os_disk {
+    name                 = coalesce(var.os_disk.name, "${var.host.name}-os-disk")
+    disk_size_gb         = var.os_disk.size
+    caching              = var.os_disk.caching
+    storage_account_type = var.os_disk.storage_account_type
+  }
+
+  boot_diagnostics {
+    storage_account_uri = var.host.diag_storage_account_uri
+  }
+
+  availability_set_id = try(var.availability_set.id, null)
+
+  lifecycle {
+    ignore_changes = [
+      # Needs to be ignored to prevent recreation when changes are made to the cloud init config
+      custom_data
+    ]
+  }
+}
+
+# Windows VM
+resource "azurerm_windows_virtual_machine" "this" {
+  count               = var.host.is_windows ? 1 : 0
+  resource_group_name = var.resource_group_name
+
+  name           = var.host.name
+  computer_name  = coalesce(var.host.computer_name, var.host.name)
+  size           = var.host.size
+  location       = var.location
+  admin_username = "imadmin"
+  admin_password = random_password.this.result
+  tags           = merge({ "Omnia-OS-Type" = "WS2022" }, var.vm_only_tags, var.tags)
+  license_type   = "Windows_Server"
+
+  patch_assessment_mode = "AutomaticByPlatform"
+  patch_mode            = "AutomaticByPlatform"
+
+  bypass_platform_safety_checks_on_user_schedule_enabled = true
+  provision_vm_agent                                     = true
+
+  network_interface_ids = [
+    azurerm_network_interface.this.id
+  ]
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  source_image_reference {
+    publisher = try(var.source_image.publisher, "MicrosoftWindowsServer")
+    offer     = try(var.source_image.offer, "WindowsServer")
+    sku       = try(var.source_image.sku, "2022-Datacenter-smalldisk")
+    version   = try(var.source_image.version, "latest")
+  }
+
+  os_disk {
+    name                 = coalesce(var.os_disk.name, "${var.host.name}-os-disk")
+    disk_size_gb         = var.os_disk.size
+    caching              = var.os_disk.caching
+    storage_account_type = var.os_disk.storage_account_type
+  }
+
+  boot_diagnostics {
+    storage_account_uri = var.host.diag_storage_account_uri
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,15 +1,90 @@
 variable "resource_group_name" {
-  description = "The name of the resource group to create the resources in."
+  description = "The name of the resource group to create this virtual machine in."
   type        = string
 }
 
 variable "location" {
-  description = "The location to create the resources in."
+  description = "The location to create this virtual machine in."
   type        = string
 }
 
 variable "tags" {
-  description = "A map of tags to assign to the resources."
+  description = "A map of tags to assign to this virtual machine."
   type        = map(string)
   default     = {}
+}
+
+variable "vm_name" {
+  description = "The name to be given to this virtual machine"
+  type        = string
+}
+
+variable "vm_computer_name" {
+  description = "The computer name to be given to this virtual machine, default is the vm_name"
+  type        = string
+  default     = null
+}
+
+variable "vm_size" {
+  description = "What size should the virtual machine be, default is Standard_D2s_v4"
+  type        = string
+  default     = "Standard_D2s_v4"
+}
+
+variable "is_windows" {
+  description = "Should this virtual machine run Windows, default is linux"
+  type        = bool
+  default     = false
+}
+
+variable "os_disk" {
+  description = "The OS disk for this VM"
+  type = object({
+    name                 = optional(string, "")
+    size                 = optional(number, 256)
+    caching              = optional(string, "ReadWrite")
+    storage_account_type = optional(string, "Premium_LRS")
+  })
+}
+
+variable "data_disks" {
+  description = "data disks for this virtual machine"
+  type = map(
+    object({
+      name                 = optional(string, "")
+      storage_account_type = optional(string, "Premium_LRS")
+      create_option        = optional(string, "Empty")
+      source_resource_id   = optional(string, "")
+      storage_account_id   = optional(string, null)
+      hyper_v_generation   = optional(string, null)
+      size                 = optional(number, 1024)
+    })
+  )
+}
+
+variable "network_interfaces" {
+  description = "All network interfaces for this virtual machine"
+  type = map(object({
+    ip_configuration = object({
+      subnet_id             = string
+      nsg_group_id          = optional(string, null)
+      ip_address_allocation = optional(string, "Dynamic")
+      ip_config_name        = optional(string, null)
+      dns_servers           = optional(list(string), null)
+    })
+  }))
+}
+
+variable "boot_diagnostics_storage_account_uri" {
+  description = "The Primary/Secondary Endpoint for the Azure Storage Account which should be used to store Boot Diagnostics, including Console Output and Screenshots from the Hypervisor."
+  type        = string
+  default     = null
+}
+
+variable "availability_set_id" {
+  description = "Specifies the ID of the Availability Set in which this Virtual Machine should exist"
+  type = object({
+    id = string
+  })
+  default = null
 }
