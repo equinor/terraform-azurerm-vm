fe8c1139952ff663190d0aa594ced7af0cd44b11
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-03-08 08:18:26 +0100

    Update example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -14,14 +14,6 @@ resource "random_id" "suffix" {
   byte_length = 8
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "nsg-${random_id.suffix.hex}"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-
-  tags = local.tags
-}
-
 module "log_analytics" {
   source  = "equinor/log-analytics/azurerm"
   version = "2.2.0"
@@ -29,28 +21,6 @@ module "log_analytics" {
   workspace_name      = "log-${random_id.suffix.hex}"
   resource_group_name = var.resource_group_name
   location            = var.location
-
-  tags = local.tags
-}
-
-module "network" {
-  source  = "equinor/network/azurerm"
-  version = "3.0.0"
-
-  vnet_name           = "vnet-${random_id.suffix.hex}"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  address_spaces      = ["10.0.0.0/16"]
-
-  subnets = {
-    "vm" = {
-      name             = "snet-vm-${random_id.suffix.hex}"
-      address_prefixes = ["10.0.1.0/24"]
-      network_security_group = {
-        id = azurerm_network_security_group.example.id
-      }
-    }
-  }
 
   tags = local.tags
 }
@@ -67,6 +37,25 @@ module "storage" {
   tags = local.tags
 }
 
+module "network" {
+  source  = "equinor/network/azurerm"
+  version = "3.1.0"
+
+  vnet_name           = "vnet-${random_id.suffix.hex}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  address_spaces      = ["10.0.0.0/16"]
+
+  subnets = {
+    "vm" = {
+      name             = "snet-vm-${random_id.suffix.hex}"
+      address_prefixes = ["10.0.1.0/24"]
+    }
+  }
+
+  tags = local.tags
+}
+
 module "vm" {
   # source = "equinor/vm/azurerm"
   # version = "0.0.0"
@@ -75,13 +64,14 @@ module "vm" {
   vm_name               = "vm-${random_id.suffix.hex}"
   resource_group_name   = var.resource_group_name
   location              = var.location
-  storage_blob_endpoint = module.storage.blob_endpoint
   admin_username        = "azureadminuser"
   os_disk_name          = "osdisk-vm-${random_id.suffix.hex}"
+  storage_blob_endpoint = module.storage.blob_endpoint
 
   network_interfaces = {
-    "backend" = {
+    "default" = {
       name = "nic-vm-${random_id.suffix.hex}"
+
       ip_configurations = [
         {
           name      = "ipconfig1"
